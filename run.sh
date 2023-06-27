47bcf5273560bd957a93c02852ff9ec352ca168d
Matthieu Pigaglio <matthieu.pigaglio@uclouvain.be>
2023-06-27 13:45:35 +0200

    up

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $1 == "bootstrap" ]]; then
-    go run . -seed 1234 -port 61960 -duration $2&
+    go run . -seed 1234 -port 50000 -duration $2&
 else
-    go run . -peer /ip4/10.210.118.52/tcp/61960/p2p/12D3KooWE3AwZFT9zEWDUxhya62hmvEbRxYBWaosn7Kiqw5wsu73 -duration $1 &
+    go run . -peer /ip4/10.210.118.52/tcp/50000/p2p/12D3KooWE3AwZFT9zEWDUxhya62hmvEbRxYBWaosn7Kiqw5wsu73 -duration $1 &
 fi
 
 # Wait for the code to finish and exit
